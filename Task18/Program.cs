86da3dc543f26ca0a2b0be6f38e927a81c68885c
Pavel <pasha-pro2014@yandex.ru>
2023-07-27 12:22:31 +0300

    Seminar_3

---
+++ Task18/Program.cs
@@ -0,0 +1,25 @@
+﻿// 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+Console.Write("Введите номер четверти: ");
+string? numberQuarter = Console.ReadLine();
+
+string? range = "";
+
+if(numberQuarter != null)
+    range = RangeQuarter(numberQuarter);
+
+if (range != null) 
+    Console.WriteLine(range);
+else Console.WriteLine("Некорректная четверть"); 
+
+string? RangeQuarter (string numQuarter)
+{
+    if(numQuarter == "1") return "x > 0, y > 0";
+    if(numQuarter == "2") return "x < 0, y > 0";
+    if(numQuarter == "3") return "x < 0, y < 0";
+    if(numQuarter == "4") return "x > 0, y < 0";
+    return null;
+}
---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректные данные");
+    return;
+}
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int square = i * i;
+        Console.WriteLine($"{i, 3} {square, 5}");
+    }
+}
