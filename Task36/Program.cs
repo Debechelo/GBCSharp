073302836495f913f75c2fbf8d9d66c83c6a7484
Pavel <pasha-pro2014@yandex.ru>
2023-08-04 16:55:13 +0300

    Homework_ 5

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 36: Задайте одномерный массив, заполненный случайными
+//  числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+int SumOddIndexNumbers(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(10, -10, 9);
+PrintArray(array);
+
+int result = SumOddIndexNumbers(array);
+Console.WriteLine($"Сумма чисел на нечетных позициях в массиве: {result}");
+
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным
+//  и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+
+    return arr;
+}
+
+void PrintArray(double[] arr, int round = 1)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double roundNum = Math.Round(arr[i], round);
+
+        Console.Write(roundNum);
+        if (i < arr.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+double DiffMinMax(double[] arr)
+{
+    if (arr.Length == 0)
+        return 0;
+
+    double min = arr[0];
+    double max = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    return max - min;
+}
+
+double[] array = CreateArrayRndDouble(10, 0, 99);
+PrintArray(array);
+
+double result = DiffMinMax(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {Math.Round(result, 2)}");
