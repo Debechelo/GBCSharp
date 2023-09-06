0a4102287b7d42dc5c1c1c4980bb30367b9ce5ed
Pavel <pasha-pro2014@yandex.ru>
2023-09-06 16:24:33 +0300

    Homework_9

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M <= 0 || N <= 0)
+{
+    Console.Write($"Некоректное входные данные");
+    return;
+}
+
+if (M > N)
+{
+    (M, N) = (N, M);
+}
+
+int sum = RangeSum(M, N);
+Console.Write($"{sum}");
+
+int RangeSum(int m, int n)
+{
+    if (n == m - 1) return 0;
+    return n + RangeSum(m, n - 1);
+}
--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿// 68: Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите целое неотрицательное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое неотрицательное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M < 0 || N < 0)
+{
+    Console.Write($"Некоректное входные данные");
+    return;
+}
+
+int A = Akerman(M, N);
+Console.Write($"{A}");
+
+int Akerman(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (n != 0 && m == 0)
+    {
+        return Akerman(n - 1, 1);
+    }
+    else
+    {
+        return Akerman(n - 1, Akerman(n, m - 1));
+    }
+}
